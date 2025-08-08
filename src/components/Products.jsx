14c6333f66b88eb9281c67d5ced27e0068438e66
azeemabrarkhan <97536297+azeemabrarkhan@users.noreply.github.com>
2025-08-08 14:32:47 +0200

    fix skeleton height (same as product size)

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -46,22 +46,22 @@ const Products = () => {
           <Skeleton height={40} width={560} />
         </div>
         <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
-          <Skeleton height={592} />
+          <Skeleton height={540} />
         </div>
         <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
-          <Skeleton height={592} />
+          <Skeleton height={540} />
         </div>
         <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
-          <Skeleton height={592} />
+          <Skeleton height={540} />
         </div>
         <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
-          <Skeleton height={592} />
+          <Skeleton height={540} />
         </div>
         <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
-          <Skeleton height={592} />
+          <Skeleton height={540} />
         </div>
         <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
-          <Skeleton height={592} />
+          <Skeleton height={540} />
         </div>
       </>
     );
