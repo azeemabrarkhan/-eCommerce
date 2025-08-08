771fd2ab11a0a7dadd6ae03a6708c0dee1a23b44
azeemabrarkhan <97536297+azeemabrarkhan@users.noreply.github.com>
2025-08-08 15:21:07 +0200

    use ProductCard ion the products page and refactoring

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,23 +1,21 @@
 import React, { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { addCart } from "../redux/action";
-
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
+import { ProductCard } from "./";
 
-import { Link } from "react-router-dom";
-import toast from "react-hot-toast";
+const NUMBER_OF_PRODUCT_SKELETONS = 6;
+const FILTER_BUTTONS_LABELS = [
+  "All",
+  "Men's Clothing",
+  "Women's Clothing",
+  "Jewelery",
+  "Electronics",
+];
 
 const Products = () => {
   const [data, setData] = useState([]);
   const [filter, setFilter] = useState(data);
   const [loading, setLoading] = useState(false);
-
-  const dispatch = useDispatch();
-
-  const addProduct = (product) => {
-    dispatch(addCart(product));
-  };
 
   useEffect(() => {
     let isMounted = true;
@@ -39,30 +37,17 @@ const Products = () => {
     };
   }, []);
 
-  const Loading = () => {
+  const renderSkeleton = () => {
     return (
       <>
         <div className="col-12 py-5 text-center">
           <Skeleton height={40} width={560} />
         </div>
-        <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
-          <Skeleton height={540} />
-        </div>
-        <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
-          <Skeleton height={540} />
-        </div>
-        <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
-          <Skeleton height={540} />
-        </div>
-        <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
-          <Skeleton height={540} />
-        </div>
-        <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
-          <Skeleton height={540} />
-        </div>
-        <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
-          <Skeleton height={540} />
-        </div>
+        {[...Array(NUMBER_OF_PRODUCT_SKELETONS).keys()].map((number) => (
+          <div key={number} className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
+            <Skeleton height={540} />
+          </div>
+        ))}
       </>
     );
   };
@@ -72,93 +57,31 @@ const Products = () => {
     setFilter(updatedList);
   };
 
-  const ShowProducts = () => {
+  const renderProducts = () => {
     return (
       <>
         <div className="buttons text-center py-5">
-          <button
-            className="btn btn-outline-dark btn-sm m-2"
-            onClick={() => setFilter(data)}
-          >
-            All
-          </button>
-          <button
-            className="btn btn-outline-dark btn-sm m-2"
-            onClick={() => filterProduct("men's clothing")}
-          >
-            Men's Clothing
-          </button>
-          <button
-            className="btn btn-outline-dark btn-sm m-2"
-            onClick={() => filterProduct("women's clothing")}
-          >
-            Women's Clothing
-          </button>
-          <button
-            className="btn btn-outline-dark btn-sm m-2"
-            onClick={() => filterProduct("jewelery")}
-          >
-            Jewelery
-          </button>
-          <button
-            className="btn btn-outline-dark btn-sm m-2"
-            onClick={() => filterProduct("electronics")}
-          >
-            Electronics
-          </button>
-        </div>
-
-        {filter.map((product) => {
-          return (
-            <div
-              id={product.id}
-              key={product.id}
-              className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4"
+          {FILTER_BUTTONS_LABELS.map((label) => (
+            <button
+              key={label}
+              className="btn btn-outline-dark btn-sm m-2"
+              onClick={() =>
+                label === "All"
+                  ? setFilter(data)
+                  : filterProduct(label.toLowerCase())
+              }
             >
-              <div className="card text-center h-100" key={product.id}>
-                <img
-                  className="card-img-top p-3 object-fit-contain"
-                  src={product.image}
-                  alt="Card"
-                  height={300}
-                />
-                <div className="card-body">
-                  <h5 className="card-title">
-                    {product.title.substring(0, 12)}...
-                  </h5>
-                  <p className="card-text">
-                    {product.description.substring(0, 90)}...
-                  </p>
-                </div>
-                <ul className="list-group list-group-flush">
-                  <li className="list-group-item lead">$ {product.price}</li>
-                  {/* <li className="list-group-item">Dapibus ac facilisis in</li>
-                    <li className="list-group-item">Vestibulum at eros</li> */}
-                </ul>
-                <div className="card-body">
-                  <Link
-                    to={"/product/" + product.id}
-                    className="btn btn-dark m-1"
-                  >
-                    Buy Now
-                  </Link>
-                  <button
-                    className="btn btn-dark m-1"
-                    onClick={() => {
-                      toast.success("Added to cart");
-                      addProduct(product);
-                    }}
-                  >
-                    Add to Cart
-                  </button>
-                </div>
-              </div>
-            </div>
-          );
-        })}
+              {label}
+            </button>
+          ))}
+        </div>
+        {filter.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
       </>
     );
   };
+
   return (
     <>
       <div className="container my-3 py-3">
@@ -169,7 +92,7 @@ const Products = () => {
           </div>
         </div>
         <div className="row justify-content-center">
-          {loading ? <Loading /> : <ShowProducts />}
+          {loading ? renderSkeleton() : renderProducts()}
         </div>
       </div>
     </>
