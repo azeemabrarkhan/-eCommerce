80b9fdde0ac43442700423cab096c1d90a1a1550
azeemabrarkhan <97536297+azeemabrarkhan@users.noreply.github.com>
2025-08-08 14:17:21 +0200

    fix products useEffect for the unmount

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -12,7 +12,6 @@ const Products = () => {
   const [data, setData] = useState([]);
   const [filter, setFilter] = useState(data);
   const [loading, setLoading] = useState(false);
-  let componentMounted = true;
 
   const dispatch = useDispatch();
 
@@ -21,21 +20,23 @@ const Products = () => {
   };
 
   useEffect(() => {
+    let isMounted = true;
+
     const getProducts = async () => {
       setLoading(true);
       const response = await fetch("https://fakestoreapi.com/products/");
-      if (componentMounted) {
+      if (isMounted) {
         setData(await response.clone().json());
         setFilter(await response.json());
         setLoading(false);
       }
-
-      return () => {
-        componentMounted = false;
-      };
     };
 
     getProducts();
+
+    return () => {
+      isMounted = false; // cleanup function on unmount
+    };
   }, []);
 
   const Loading = () => {
